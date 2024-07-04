c21182fb87f8175fec5ed061cf01164fe8f924a7
ghimirelava <lghimire@usfca.edu>
2024-07-04 10:15:26 -0700

    updated projects lang section

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var Website = /** @class */ (function () {
         this.projects = [
             { id: 1, title: "Search Engine: Fetch", description: "Fetch is a full-stack search engine and web crawler that enables users to search for terms across public websites. The project was developed with Golang and SQL, incorporating custom data structures and TF-IDF scores to generate diverse statistics for the user.", image: "public/images/fetch.jpg", languages: ["Golang", "SQL", "Web Crawling"] },
             { id: 2, title: "Web Application Frontend", description: "Duets.AI is a language learning startup that utilizes AI to provide personalized feedback and exercises from Zoom tutoring sessions. I was one of the two front-end developers who designed and developed the frontend from scratch. Emphasis was placed on ensuring a seamless user interface and working with the back end team to integrate dynamic data through custom API calls to establish the websites core functionalities in 4 months.", image: "public/images/duets.jpg", languages: ["Bubble.io", "HTML", "Azure"] },
-            { id: 3, title: "Mini Max Game", description: "Highlight C skills and building algorithms", image: "public/images/project3.jpg", languages: ["JavaScript", "HTML", "CSS"] },
-            { id: 4, title: "OS history command or emulator project", description: "show understanding of of asm and low level concepts or os concepts", image: "public/images/project4.jpg", languages: ["Bubble.io", "HTML", "Adjuer"] },
+            { id: 3, title: "Mini Max Game", description: "Highlight C skills and building algorithms", image: "public/images/project3.jpg", languages: ["C", "HTML", "CSS"] },
+            { id: 4, title: "OS history command or emulator project", description: "Show understanding of of asm and low level concepts or os concepts", image: "public/images/project4.jpg", languages: ["C", "ASM", "RISC V"] },
         ];
         this.initializeEventListeners();
         this.loadProjects();
